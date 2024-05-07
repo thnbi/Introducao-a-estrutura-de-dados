591ef1818a6d10d6b40c57133c86ac361c6969d5
thnbi <renato_freitas13@icloud.com>
2024-05-07 14:38:55 -0300

    strings

---
+++ strings.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void getCityName()
+{
+   char cidade[81];
+   scanf(" %80[^\n]", cidade);
+   printf("%s", cidade);
+}
+
+void print(char *s)
+{
+   for (int i = 0; s[i] != '\0'; i++)
+   {
+      printf("%c", s[i]);
+   }
+   printf("\n");
+}
+
+int comprimento(char *s)
+{
+   int n = 0;
+   for (int i = 0; s[i] != '\0'; i++)
+      n++;
+   return n;
+}
+
+void copy(char *orig, char *dest)
+{
+   int i;
+   for (i = 0; orig[i] != '\0'; i++)
+      dest[i] = orig[i];
+   dest[i] = '\0';
+}
+
+void concatena(char *orig, char *dest)
+{
+   int i = 0;
+   while (dest[i] != '\0')
+      i++;
+   for (int j = 0; orig[j] != '\0'; j++)
+   {
+      dest[i] = orig[j];
+      i++;
+   }
+   dest[i] = '\0';
+}
+
+char *uppercase(char *c)
+{
+   for (int i = 0; c[i] != '\0'; i++)
+   {
+      if ((c[i] >= 'a') && (c[i] <= 'z'))
+         c[i] = c[i] - 'a' + 'A';
+   }
+   return c;
+}
+
+int compara(char *s, char *s2)
+{
+   int i;
+
+   for (i = 0; s[i] != '\0' && s2[i] != '\0'; i++)
+   {
+
+      if (s[i] < s2[i])
+         return -1;
+      else if (s[i] > s2[i])
+         return 1;
+   }
+   if (s[i] == s2[i])
+      return 0;
+   else if (s2[i] != '\0')
+      return -1;
+   else
+      return 1;
+}
+
+char *duplica(char *s)
+{
+   int n = strlen(s);
+   char *d = (char *)malloc((n + 1) * sizeof(char));
+   strcpy(d, s);
+   return d;
+}
+
+int main(void)
+{
+   return 0;
+}
