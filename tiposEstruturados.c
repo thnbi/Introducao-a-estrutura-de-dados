17308d3dd6da46dde0cc2c3a1e374effdbdb5a58
thnbi <renato_freitas13@icloud.com>
2024-05-08 17:00:53 -0300

    tipos estruturados, com ponteiros

--- tiposEstruturados.c
+++ tiposEstruturados.c
@@ -6,12 +6,23 @@ struct point
    float y;
 };
 
+void print(struct point *pp)
+{
+   printf("O ponto fornecido foi: (%.2f, %.2f)\n", pp->x, pp->y);
+}
+
+void read(struct point *pp)
+{
+   printf("Digite as cordenadas do ponto(x, y): ");
+   scanf("%f %f", &pp->x, &pp->y);
+}
+
 int main(void)
 {
    struct point p;
 
-   printf("Digite as cordenadas do ponto(x, y): ");
-   scanf("%f %f", &p.x, &p.y);
-   printf("O ponto fornecido foi: (%.2f, %.2f)\n", p.x, p.y);
+   read(&p);
+   print(&p);
+
    return 0;
 }
