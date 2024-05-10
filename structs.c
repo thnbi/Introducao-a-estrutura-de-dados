232a724762277f953e3e248e1eeeac632623225a
thnbi <renato_freitas13@icloud.com>
2024-05-10 18:20:08 -0300

    structs avancando

--- structs.c
+++ structs.c
@@ -14,10 +14,17 @@ typedef struct circle
 } Circle;
 
 float distance(Point *, Point *);
+float area(int, Point *);
 int isInside(Circle *, Point *);
+Point geometricCenter(int, Point *);
 
 int main(int argc, char **argv)
 {
+   Point p[3] = {
+       {1.0, 1.0},
+       {5.0, 1.0},
+       {4.0, 3.0}};
+   printf("area = %.2f\n", area(3, p));
    return 0;
 }
 
@@ -31,4 +38,31 @@ int isInside(Circle *c, Point *p)
 {
    float d = distance(&c->p, p);
    return (d < c->r);
+}
+
+float area(int n, Point *p)
+{
+   int i, j;
+   float a = 0;
+
+   for (i = 0; i < n; i++)
+   {
+      j = (i + 1) % n;
+      a += (p[j].x - p[i].x) * (p[i].y + p[j].y) / 2;
+   }
+
+   return fabs(a);
+}
+
+Point geometricCenter(int n, Point *v)
+{
+   Point p = {0.0f, 0.0f};
+   for (int i = 0; i < n; i++)
+   {
+      p.x += v[i].x;
+      p.y += v[i].y;
+   }
+   p.x /= n;
+   p.y /= n;
+   return p;
 }
---
+++ students.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+typedef struct student
+{
+   int mat;
+   char name[81];
+   char addr[121];
+   char phone[21];
+} Student;
+
+Student *tab[MAX];
+
+void initialize(int n, Student **tab)
+{
+   for (int i = 0; i < n; i++)
+      tab[i] = NULL;
+}
+
+void getDataFromTerm(Student **tab, int i)
+{
+   printf("Matricula: ");
+   scanf("%d", &tab[i]->mat);
+
+   printf("Nome: ");
+   scanf(" %80[^\n]", tab[i]->name);
+
+   printf("Endereco: ");
+   scanf(" %120[^\n]", tab[i]->addr);
+
+   printf("Telefone: ");
+   scanf(" %20[^\n]", tab[i]->phone);
+}
+
+int invalidVectorLimit(int n, int i)
+{
+   if (i < 0 || i >= n)
+   {
+      return 1;
+   }
+   else
+      return 0;
+}
+
+void fill(int n, Student **tab, int i)
+{
+   if (invalidVectorLimit(n, i))
+      exit(1);
+   if (tab[i] == NULL)
+      tab[i] = (Student *)malloc(sizeof(Student));
+   getDataFromTerm(tab, i);
+}
+
+void takeOff(int n, Student **tab, int i)
+{
+   if (invalidVectorLimit(n, i))
+      exit(1);
+
+   if (tab[1] != NULL)
+   {
+      free(tab[i]);
+      tab[i] = NULL;
+   }
+}
+
+void print(int n, Student **tab, int i)
+{
+   if (invalidVectorLimit(n, i))
+      exit(1);
+
+   if (tab[i] != NULL)
+   {
+      printf("Matricula: %d\n", tab[i]->mat);
+      printf("Nome: %s\n", tab[i]->name);
+      printf("Endereco: %s\n", tab[i]->addr);
+      printf("Telefone: %s\n", tab[i]->phone);
+   }
+}
+
+void printAllStudents(int n, Student **tab)
+{
+   for (int i = 0; i < n; i++)
+      print(n, tab, i);
+}
+
+int main(int argc, char **argv)
+{
+   Student *tab[10];
+   fill(10, tab, 0);
+   fill(10, tab, 1);
+   fill(10, tab, 2);
+   printAllStudents(10, tab);
+   takeOff(10, tab, 0);
+   takeOff(10, tab, 1);
+   takeOff(10, tab, 2);
+
+   return 0;
+}
